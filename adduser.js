22d30214d23c297f6a4e0ec27325c4c3ba7a1f69
Ola Gamal <ola66734@gmail.com>
2021-01-14 15:58:26 +0200

    pharmacy project

--- adduser.js
+++ adduser.js
@@ -1,5 +1,4 @@
 var db = openDatabase('pharma-db', '1.0', 'pharma db', 2 * 1024 * 1024);
-
 // db.transaction(function (tx) {
 //     tx.executeSql('DELETE FROM users where username = "sara"');
 //     // tx.executeSql('update users set username = "ola" where username = "shahenda"');
@@ -10,12 +9,12 @@ function addNewUser() {
     var password = document.getElementById("exampleInputPassword1").value;
     var confirmPassword = document.getElementById("exampleInputPassword2").value;
     var encryptedPass = MD5(password);
-    if(username.length != 0 && password.length != 0 && confirmPassword.length != 0) {
-        if(password == confirmPassword) {
+    if (username.length != 0 && password.length != 0 && confirmPassword.length != 0) {
+        if (password == confirmPassword) {
             db.transaction(function (tx) {
                 tx.executeSql('INSERT INTO users VALUES (?,?)', [username, encryptedPass]);
             });
-            alert("New user is entered");
+            alert("New user added")
         }
         else {
             alert("Unmatched passwords");
